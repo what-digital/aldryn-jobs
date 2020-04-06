2fd8cef9d02d1e727c80d07a5b707a4c0e011211
Serge Fedoruk <s.fedoruk@nephila.digital>
2020-04-06 09:20:50 +0200

    fix urls ordering

--- aldryn_jobs/urls.py
+++ aldryn_jobs/urls.py
@@ -19,6 +19,21 @@ from .views import (
 DEFAULT_VIEW = 'job-opening-list'
 
 urlpatterns = [
+    url(r'^confirm-newsletter/$',
+        RegisterJobNewsletter.as_view(),
+        name="register_newsletter"),
+    url(r'^confirm-newsletter/registration-notification/$',
+        SuccessRegistrationMessage.as_view(),
+        name="newsletter_registration_notification"),
+    url(r'^confirm-newsletter/(?P<key>\w+)/$',
+        ConfirmNewsletterSignup.as_view(),
+        name="confirm_newsletter_email"),
+    url(r'^unsubscribe-newsletter/(?P<key>\w+)/$',
+        UnsubscibeNewsletterSignup.as_view(),
+        name="unsubscribe_from_newsletter"),
+    url(r'^resend-newsletter-confirmation/(?P<key>\w+)/$',
+        ResendNewsletterConfirmation.as_view(),
+        name="resend_confirmation_link"),
     url(r'^$', JobOpeningList.as_view(),
         name='job-opening-list'),
     url(r'^(?P<category_slug>\w[-_\w]*)/$',
@@ -27,23 +42,4 @@ urlpatterns = [
     url(r'^(?P<category_slug>\w[-_\w]*)/(?P<job_opening_slug>\w[-_\w]*)/$',
         JobOpeningDetail.as_view(),
         name='job-opening-detail'),
-    url(r'^confirm-newsletter/$',
-        RegisterJobNewsletter.as_view(),
-        name="register_newsletter"),
-
-    url(r'^confirm-newsletter/registration-notification/$',
-        SuccessRegistrationMessage.as_view(),
-        name="newsletter_registration_notification"),
-
-    url(r'^confirm-newsletter/(?P<key>\w+)/$',
-        ConfirmNewsletterSignup.as_view(),
-        name="confirm_newsletter_email"),
-
-    url(r'^unsubscribe-newsletter/(?P<key>\w+)/$',
-        UnsubscibeNewsletterSignup.as_view(),
-        name="unsubscribe_from_newsletter"),
-
-    url(r'^resend-newsletter-confirmation/(?P<key>\w+)/$',
-        ResendNewsletterConfirmation.as_view(),
-        name="resend_confirmation_link"),
 ]
