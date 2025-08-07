55b57aaac8b1abb6878125008c52d7309a06b6be
dvanser <ivans.dorohovs@gmail.com>
2025-08-07 17:50:10 +0300

    udpate urls

--- aldryn_jobs/urls.py
+++ aldryn_jobs/urls.py
@@ -2,7 +2,7 @@
 
 from __future__ import unicode_literals
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import (
     CategoryJobOpeningList,
@@ -19,27 +19,27 @@ from .views import (
 DEFAULT_VIEW = 'job-opening-list'
 
 urlpatterns = [
-    url(r'^confirm-newsletter/$',
+    re_path(r'^confirm-newsletter/$',
         RegisterJobNewsletter.as_view(),
         name="register_newsletter"),
-    url(r'^confirm-newsletter/registration-notification/$',
+    re_path(r'^confirm-newsletter/registration-notification/$',
         SuccessRegistrationMessage.as_view(),
         name="newsletter_registration_notification"),
-    url(r'^confirm-newsletter/(?P<key>\w+)/$',
+    re_path(r'^confirm-newsletter/(?P<key>\w+)/$',
         ConfirmNewsletterSignup.as_view(),
         name="confirm_newsletter_email"),
-    url(r'^unsubscribe-newsletter/(?P<key>\w+)/$',
+    re_path(r'^unsubscribe-newsletter/(?P<key>\w+)/$',
         UnsubscibeNewsletterSignup.as_view(),
         name="unsubscribe_from_newsletter"),
-    url(r'^resend-newsletter-confirmation/(?P<key>\w+)/$',
+    re_path(r'^resend-newsletter-confirmation/(?P<key>\w+)/$',
         ResendNewsletterConfirmation.as_view(),
         name="resend_confirmation_link"),
-    url(r'^$', JobOpeningList.as_view(),
+    re_path(r'^$', JobOpeningList.as_view(),
         name='job-opening-list'),
-    url(r'^(?P<category_slug>\w[-_\w]*)/$',
+    re_path(r'^(?P<category_slug>\w[-_\w]*)/$',
         CategoryJobOpeningList.as_view(),
         name='category-job-opening-list'),
-    url(r'^(?P<category_slug>\w[-_\w]*)/(?P<job_opening_slug>\w[-_\w]*)/$',
+    re_path(r'^(?P<category_slug>\w[-_\w]*)/(?P<job_opening_slug>\w[-_\w]*)/$',
         JobOpeningDetail.as_view(),
         name='job-opening-detail'),
 ]
